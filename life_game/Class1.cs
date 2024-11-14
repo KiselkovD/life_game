818992d352fbe253a3319793cccb0fd3f18c5559
Denis <denis.kiselkov@list.ru>
2024-11-14 08:10:23 +0300

    format

--- life_game/Class1.cs
+++ life_game/Class1.cs
@@ -4,48 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
-
 namespace coursework_wpf
 {
     public class Const
     { public const int MAX_GRID = 12; }
-        
-
     public class Cell  // клетка
     {
-        public BitArray CellStateFlags = new BitArray(5); 
-
+        public BitArray CellStateFlags = new BitArray(5);
         public override string ToString()
         {
             return $"{CellStateFlags} {GetCellType()}";
         }
-
         public virtual void activate(int pos, ref Cell[] arr)
         {
-            
-                int xx = pos % Const.MAX_GRID;
-                int yy = (pos - xx) / Const.MAX_GRID;
-                int ix = xx - 1, ix2 = xx + 1, iy = yy - 1, iy2 = yy + 1;
-                int howmany = 0;
-                if (ix < 0) ix = 0;
-                if (iy < 0) iy = 0;
-                if (ix2 > Const.MAX_GRID - 1) ix2 = Const.MAX_GRID - 1;
-                if (iy2 > Const.MAX_GRID - 1) iy2 = Const.MAX_GRID - 1;
-                for (int t1 = ix; t1 < ix2 + 1; t1++)
+            int xx = pos % Const.MAX_GRID;
+            int yy = (pos - xx) / Const.MAX_GRID;
+            int ix = xx - 1, ix2 = xx + 1, iy = yy - 1, iy2 = yy + 1;
+            int howmany = 0;
+            if (ix < 0) ix = 0;
+            if (iy < 0) iy = 0;
+            if (ix2 > Const.MAX_GRID - 1) ix2 = Const.MAX_GRID - 1;
+            if (iy2 > Const.MAX_GRID - 1) iy2 = Const.MAX_GRID - 1;
+            for (int t1 = ix; t1 < ix2 + 1; t1++)
+            {
+                for (int t2 = iy; t2 < iy2 + 1; t2++)
                 {
-                    for (int t2 = iy; t2 < iy2 + 1; t2++)
-                    {
-                        if (arr[t1 + t2 * Const.MAX_GRID].GetCellType() == 1) howmany++;
-                    }
+                    if (arr[t1 + t2 * Const.MAX_GRID].GetCellType() == 1) howmany++;
                 }
-                if (howmany == 3) arr[pos] = new usual();
-            
+            }
+            if (howmany == 3) arr[pos] = new usual();
         }
         public virtual void spawn(int pos, ref Cell[] arr)
         {
-            if (arr[pos].CellStateFlags[0]) arr[pos] = new killer();  
+            if (arr[pos].CellStateFlags[0]) arr[pos] = new killer();
             else
             {
                 if (arr[pos].CellStateFlags[1]) arr[pos] = new usual();
@@ -57,7 +48,6 @@ namespace coursework_wpf
                         if (arr[pos].CellStateFlags[3]) arr[pos] = new helther();
                         else
                         {
-
                         }
                     }
                 }
@@ -65,20 +55,17 @@ namespace coursework_wpf
             arr[pos].CellStateFlags = new BitArray(5);
             for (int bi = 0; bi < 5; bi++) CellStateFlags[bi] = false;
         }
-        public virtual int GetCellType ()
+        public virtual int GetCellType()
         { return 5; }
-
-        
     }
-
     public class killer : Cell // клетка  
     {
         public override void activate(int pos, ref Cell[] arr)
         {
-            int xx =pos % Const.MAX_GRID;
+            int xx = pos % Const.MAX_GRID;
             int yy = (pos - xx) / Const.MAX_GRID;
-            int ix=xx-2, ix2=xx+2, iy=yy-2, iy2=yy+2;
-            bool fl_d=false;
+            int ix = xx - 2, ix2 = xx + 2, iy = yy - 2, iy2 = yy + 2;
+            bool fl_d = false;
             if (ix < 0) ix = 0;
             if (iy < 0) iy = 0;
             if (ix2 > Const.MAX_GRID - 1) ix2 = Const.MAX_GRID - 1;
@@ -109,19 +96,14 @@ namespace coursework_wpf
             {
                 arr[pos] = new Cell();
             }
-
-
             //убийцы – 
             //любая живность кроме убийцы в радиусе двух клеток -> окружает себя убийцами иначе погибает
-
         }
-
         public override void spawn(int pos, ref Cell[] arr)
         {
             arr[pos].CellStateFlags = new BitArray(5);
             for (int bi = 0; bi < 5; bi++) CellStateFlags[bi] = false;
         }
-
         public override int GetCellType()
         { return 0; }
     }
@@ -148,7 +130,6 @@ namespace coursework_wpf
             // обычные – 
             //три обычные вокруг пустой -> становится обычной
             //2 - 3 соседа у обычной->живет дальше
-
         }
         public override void spawn(int pos, ref Cell[] arr)
         {
@@ -187,10 +168,10 @@ namespace coursework_wpf
             {
                 for (int t2 = iy; t2 < iy2 + 1; t2++)
                 {
-                    if (arr[t1 + t2 * Const.MAX_GRID].GetCellType() == 5 || arr[t1 + t2 * Const.MAX_GRID].GetCellType() == 4 ) fl_d2++;
+                    if (arr[t1 + t2 * Const.MAX_GRID].GetCellType() == 5 || arr[t1 + t2 * Const.MAX_GRID].GetCellType() == 4) fl_d2++;
                 }
             }
-            if (fl_d && (fl_d2>2))
+            if (fl_d && (fl_d2 > 2))
             {
                 ix = xx - 1; ix2 = xx + 1; iy = yy - 1; iy2 = yy + 1;
                 if (ix < 0) ix = 0;
@@ -204,7 +185,6 @@ namespace coursework_wpf
                         arr[t1 + t2 * Const.MAX_GRID].CellStateFlags[2] = true;
                     }
                 }
-
             }
             else
             {
@@ -212,11 +192,9 @@ namespace coursework_wpf
             }
             //           социальные – 
             //любая живность кроме убийцы в радиусе двух клеток и три свободные ячейки -> окружает себя по диагоналям социальными иначе погибает
-
         }
         public override void spawn(int pos, ref Cell[] arr)
         {
-            
             arr[pos].CellStateFlags = new BitArray(5);
             for (int bi = 0; bi < 5; bi++) CellStateFlags[bi] = false;
         }
@@ -231,8 +209,6 @@ namespace coursework_wpf
             int yy = (pos - xx) / Const.MAX_GRID;
             //          жиживучие – 
             //если рядом есть живучий и одна не занятая клетка -> создает еще одного иначе погибает
-            
-
         }
         public override void spawn(int pos, ref Cell[] arr)
         {
@@ -245,10 +221,10 @@ namespace coursework_wpf
     public class food : Cell // клетка 
     {
         public override void activate(int pos, ref Cell[] arr)
-        {    }
+        { }
         public override void spawn(int pos, ref Cell[] arr)
         {
-            if (arr[pos].CellStateFlags[0]) arr[pos] = new killer();  
+            if (arr[pos].CellStateFlags[0]) arr[pos] = new killer();
             else
             {
                 if (arr[pos].CellStateFlags[1]) arr[pos] = new usual();
@@ -260,7 +236,6 @@ namespace coursework_wpf
                         if (arr[pos].CellStateFlags[3]) arr[pos] = new helther();
                         else
                         {
-
                         }
                     }
                 }
@@ -271,34 +246,26 @@ namespace coursework_wpf
         public override int GetCellType()
         { return 4; }
     }
-
-    public class Net 
+    public class Net
     {
-
         public Cell[] Cells;
-
-        public Net() 
+        public Net()
         {
             Cells = new Cell[Const.MAX_GRID * Const.MAX_GRID];
             for (int t = 0; t < Const.MAX_GRID * Const.MAX_GRID; t++)
-                {
+            {
                 //Console.Write(" ");
-                    Cells[t] = new Cell();
-                }
-        
+                Cells[t] = new Cell();
+            }
         }
-        
-    public override string ToString()
+        public override string ToString()
         {
             String x = "";
-            for (int t=0;t< Const.MAX_GRID * Const.MAX_GRID; t++)
+            for (int t = 0; t < Const.MAX_GRID * Const.MAX_GRID; t++)
             {
                 x += $"{Cells[t]} ";
             }
             return x;
         }
-        
-
-        
     }
 }
--- life_game/MainWindow.xaml.cs
+++ life_game/MainWindow.xaml.cs
@@ -14,21 +14,19 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using System.IO;
-
 namespace coursework_wpf
 {
-
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string[] CellColors = new string[6] { "#FFDC143C", "#FF560074","#FFADD8E6","#FF008000","#FFA52A2A","#FF36393F" };
+        public string[] CellColors = new string[6] { "#FFDC143C", "#FF560074", "#FFADD8E6", "#FF008000", "#FFA52A2A", "#FF36393F" };
         public int[] array = new int[1] { 1 };
         public int cell_type = 0;
         public int input_type = 0;
         public double speed = 0;
-        public bool fl_pause =true,fl_menu = true;
+        public bool fl_pause = true, fl_menu = true;
         public int buff = 666;
         public int buff2 = 0;
         public int sav_buf = 1;
@@ -42,7 +40,6 @@ namespace coursework_wpf
             int tt = 1;
             foreach (UIElement el in gameField.Children)//создаем события для работы с сеткой
             {
-
                 if (el is Button)
                 {
                     ((Button)el).Click += Button_Click;
@@ -55,20 +52,18 @@ namespace coursework_wpf
                 if (load.Children[dd] is Button)
                 {
                     ((Button)load.Children[dd]).Click += Button_loads_Click;
-}
+                }
                 if (save.Children[dd] is Button)
-                { 
+                {
                     ((Button)save.Children[dd]).Click += Button_saves_Click;
                 }
             }
-           
-            Console.WriteLine() ;
+            Console.WriteLine();
             InitializeTimer();
-
         }
         private void InitializeTimer()
         {
-            var interval = TimeSpan.FromSeconds(0.1+speed).Ticks;
+            var interval = TimeSpan.FromSeconds(0.1 + speed).Ticks;
             gameloop = new DispatcherTimer
             {
                 Interval = TimeSpan.FromTicks(interval)
@@ -78,13 +73,12 @@ namespace coursework_wpf
         }
         private void MainGameLoop(object sender, EventArgs e)
         {
-            if(speed != 0)
+            if (speed != 0)
             {
-                 for (int ii = 0; ii < Const.MAX_GRID * Const.MAX_GRID; ii++) net.Cells[ii].activate(ii, ref net.Cells);
-                 for (int ii = 0; ii < Const.MAX_GRID * Const.MAX_GRID; ii++) net.Cells[ii].spawn(ii, ref net.Cells);
+                for (int ii = 0; ii < Const.MAX_GRID * Const.MAX_GRID; ii++) net.Cells[ii].activate(ii, ref net.Cells);
+                for (int ii = 0; ii < Const.MAX_GRID * Const.MAX_GRID; ii++) net.Cells[ii].spawn(ii, ref net.Cells);
             }
             for (int ii = 0; ii < Const.MAX_GRID * Const.MAX_GRID; ii++) ((Button)gameField.Children[ii]).Background = (Brush)converter.ConvertFromString(CellColors[net.Cells[ii].GetCellType()]);
-
         }
         //coursework_wpf.MainWindow
         BrushConverter converter = new System.Windows.Media.BrushConverter();
@@ -116,7 +110,6 @@ namespace coursework_wpf
                             break;
                     }
                 }
-
             }//читаем во 2ю
             for (int ii = 0; ii < Const.MAX_GRID * Const.MAX_GRID; ii++) ((Button)saveField.Children[ii]).Background = (Brush)converter.ConvertFromString(CellColors[net_buf.Cells[ii].GetCellType()]);
             sav_buf = Convert.ToInt32(((Button)e.OriginalSource).Content);
@@ -129,11 +122,8 @@ namespace coursework_wpf
                 for (int lol = 0; lol < 144; lol++)
                 {
                     sr.WriteLine(net.Cells[lol].GetCellType());
-
                 }
             }
-            
-
         }
         private void Button_loads_Click(object sender, RoutedEventArgs e)
         {
@@ -163,11 +153,9 @@ namespace coursework_wpf
                             break;
                     }
                 }
-
             }
             //читаем во 2ю
             for (int ii = 0; ii < Const.MAX_GRID * Const.MAX_GRID; ii++) ((Button)loadField.Children[ii]).Background = (Brush)converter.ConvertFromString(CellColors[net_buf.Cells[ii].GetCellType()]);
-
             //рисуем
             lod_buf = Convert.ToInt32(((Button)e.OriginalSource).Content);
         }
@@ -198,7 +186,6 @@ namespace coursework_wpf
                 }
             }
         }
-        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             switch (input_type)
@@ -211,7 +198,7 @@ namespace coursework_wpf
                             net.Cells[Convert.ToInt32(((Button)e.OriginalSource).FontSize) - 1] = new killer();
                             break;
                         case 1:
-                            ((Button)e.OriginalSource).Background = (Brush)converter.ConvertFromString("#FF560074"); 
+                            ((Button)e.OriginalSource).Background = (Brush)converter.ConvertFromString("#FF560074");
                             net.Cells[Convert.ToInt32(((Button)e.OriginalSource).FontSize) - 1] = new usual();
                             break;
                         case 2:
@@ -233,7 +220,7 @@ namespace coursework_wpf
                     }
                     break;
                 case 1:
-                    if (buff == 666)buff = Convert.ToInt32(((Button)e.OriginalSource).FontSize)-1; 
+                    if (buff == 666) buff = Convert.ToInt32(((Button)e.OriginalSource).FontSize) - 1;
                     else
                     {
                         buff2 = Convert.ToInt32(((Button)e.OriginalSource).FontSize) - 1;
@@ -247,30 +234,29 @@ namespace coursework_wpf
                         int yy2 = (buff2 - xx2) / Const.MAX_GRID;
                         if (xx > xx2)
                         {
-                             xxma = xx;
-                             xxmi = xx2;
+                            xxma = xx;
+                            xxmi = xx2;
                         }
                         else
                         {
-                             xxmi = xx;
-                             xxma = xx2;
+                            xxmi = xx;
+                            xxma = xx2;
                         }
                         if (yy > yy2)
                         {
-                             yyma = yy;
-                             yymi = yy2;
+                            yyma = yy;
+                            yymi = yy2;
                         }
                         else
                         {
-                             yymi = yy;
-                             yyma = yy2;
+                            yymi = yy;
+                            yyma = yy2;
                         }
-                        
-                        for (int xg = xxmi; xg < xxma+1; xg++)
+                        for (int xg = xxmi; xg < xxma + 1; xg++)
                         {
-                            for (int yg = yymi; yg < yyma+1; yg++)
+                            for (int yg = yymi; yg < yyma + 1; yg++)
                             {
-                                 switch (cell_type)
+                                switch (cell_type)
                                 {
                                     case 0:
                                         net.Cells[xg + yg * Const.MAX_GRID] = new killer();
@@ -291,16 +277,14 @@ namespace coursework_wpf
                                         net.Cells[xg + yg * Const.MAX_GRID] = new Cell();
                                         break;
                                 }
-                                 
                             }
                         }
                         buff = 666;
                     }
-                    break; 
+                    break;
                 case 2:
                     Console.WriteLine(" s = 2"); // Выполнится, если s равно 2
                     break;
-                   
             }
         }
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
@@ -315,13 +299,9 @@ namespace coursework_wpf
                 gameWin.Visibility = Visibility.Collapsed;
                 ruleWin.Visibility = Visibility.Collapsed;
             }
-
         }
         private void mouse_Click(object sender, MouseEventArgs e)
         {
-
-            
-
             Rectangle el = new Rectangle();
             el.Width = 50;
             el.Height = 50;
@@ -329,12 +309,9 @@ namespace coursework_wpf
             el.Fill = Brushes.Green;
             el.Stroke = Brushes.Red;
             el.StrokeThickness = 3;
-           
             ((Button)e.OriginalSource).Background = (Brush)converter.ConvertFromString("#FF560074");
             Background = (Brush)converter.ConvertFromString("#FF560074");
         }
-
-
         private void Button_u(object sender, RoutedEventArgs e)
         {
             cell_type = 1;
@@ -371,7 +348,6 @@ namespace coursework_wpf
         {
             input_type = 2;
         }
-        
         private void speed0(object sender, RoutedEventArgs e)
         {
             speed = 0;
@@ -384,7 +360,6 @@ namespace coursework_wpf
         {
             speed = 0.5;
         }
-
         private void menu_Click_1(object sender, RoutedEventArgs e)
         {
             fl_menu = false;
@@ -395,7 +370,6 @@ namespace coursework_wpf
             ruleWin.Visibility = Visibility.Collapsed;
             //новая игра
         }
-
         private void menu_Click_2(object sender, RoutedEventArgs e)
         {
             fl_menu = false;
@@ -406,7 +380,6 @@ namespace coursework_wpf
             ruleWin.Visibility = Visibility.Collapsed;
             //сохранить
         }
-
         private void menu_Click_3(object sender, RoutedEventArgs e)
         {
             fl_menu = false;
@@ -417,7 +390,6 @@ namespace coursework_wpf
             ruleWin.Visibility = Visibility.Collapsed;
             //загрузить
         }
-
         private void menu_Click_4(object sender, RoutedEventArgs e)
         {
             fl_menu = false;
@@ -428,20 +400,14 @@ namespace coursework_wpf
             ruleWin.Visibility = Visibility.Visible;
             //правила
         }
-
         private void menu_Click_5(object sender, RoutedEventArgs e)
         {
             //выход
             Application.Current.Shutdown();
         }
-
-       
-        
-
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             int d = ((MainWindow)Application.Current.MainWindow).array[0];
-
             MessageBox.Show(d.ToString());
         }
     }
